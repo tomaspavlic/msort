b6a7802b2e65b707c7a8ff179717f16b76a45391
Tomáš Pavlič <tomaspavlic@gmail.com>
2025-01-10 21:33:02 +0100

    remove callback heap allocation

--- src/macos/fs.rs
+++ src/macos/fs.rs
@@ -10,10 +10,6 @@ use std::{
     path::Path,
 };
 
-struct CopyFileContext {
-    progress_callback: Box<dyn Fn(i32)>,
-}
-
 pub fn copy<P, F>(from: P, to: P, progress_callback: F) -> anyhow::Result<()>
 where
     P: AsRef<Path>,
@@ -22,20 +18,17 @@ where
     let state = unsafe { copyfile_state_alloc() };
     let from = CString::new(from.as_ref().to_str().context("invalid path")?)?;
     let to = CString::new(to.as_ref().to_str().context("invalid path")?)?;
-    let context = CopyFileContext {
-        progress_callback: Box::new(progress_callback),
-    };
 
     unsafe {
         copyfile_state_set(
             state,
             COPYFILE_STATE_STATUS_CTX,
-            &context as *const _ as *const c_void,
+            &progress_callback as *const _ as *const c_void,
         );
         copyfile_state_set(
             state,
             COPYFILE_STATE_STATUS_CB,
-            copyfile_callback as *const c_void,
+            copyfile_callback::<F> as *const c_void,
         );
         let result_code = copyfile(from.as_ptr(), to.as_ptr(), state, COPYFILE_ALL);
         if result_code == 0 {
@@ -46,16 +39,19 @@ where
     }
 }
 
-fn copyfile_callback(
+fn copyfile_callback<F>(
     event: u32,
     stage: u32,
     state: *mut _copyfile_state,
     _from: *const c_char,
     _to: *const c_char,
     context: *const c_void,
-) -> u32 {
+) -> u32
+where
+    F: Fn(i32) + 'static,
+{
     if event == COPYFILE_COPY_DATA && stage == COPYFILE_PROGRESS {
-        let context = unsafe { &*(context as *const CopyFileContext) };
+        let callback = unsafe { &*(context as *const F) };
         let mut copied_bytes: c_int = 0;
         let return_code = unsafe {
             copyfile_state_get(
@@ -66,7 +62,7 @@ fn copyfile_callback(
         };
 
         if return_code == 0 {
-            (context.progress_callback)(copied_bytes);
+            (callback)(copied_bytes);
         }
     }
 
